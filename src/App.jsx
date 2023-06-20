05c61a5a9a3db98c78e2acbc894967eb4ce73476
unmukt-singh <unmukts4@gmail.com>
2023-06-20 19:52:06 +0530

    Added task-4

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import ColorPicker from "./components/ColorPicker"
+ 
+function App() {
+  return (
+    <div>
+      <ColorPicker />
+    </div>
+  );
+}
+ 
+export default App;
